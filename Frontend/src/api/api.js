eebde12c339c8281de915c8e300ee3d0a876e069
Theghost1008 <virajchronos@gmail.com>
2025-04-19 04:11:19 +0530

    debugging 9

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:"https://robomania-frontend.onrender.com/api",
+    baseURL:"https://robomania-backend.onrender.com/api",
     withCredentials: true,
 })
 
