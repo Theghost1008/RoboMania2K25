52bf52cf00b72b10bbc1d2dc8ce4ce87efddcbba
Theghost1008 <virajchronos@gmail.com>
2025-04-19 03:26:39 +0530

    debugging 2

--- Backend/app.js
+++ Backend/app.js
@@ -42,7 +42,7 @@ app.use(session({
 const server = http.createServer(app);
 const io= new Server(server,{
   cors:{
-    origin:"http://localhost:5173",
+    origin:"https://robomania-frontend.onrender.com",
     credentials: true,
   }
 })
